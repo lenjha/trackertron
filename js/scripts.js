3a3e1c9b7c5b78c055b2273950f01dd17df6cb41
Guest User <Guest@Epicodus-10D.local>
2017-10-20 10:41:17 -0700

    set variables for the radios and select boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $("#submit-button").click(function(event){
     var name = $("input#name").val();
-    // var exp = parseInt($("input#exp").val());
+    var aesthetics = $("select#aesthetics").val();
+    var syntax = $("select#syntax").val();
+    var popularity =$("input:radio[name=popularity]:checked"].val());
+    var tolerance =$("input:radio[name=tolerance]:checked"].val());
+    var stability =$("input:radio[name=stability]:checked"].val());
 
       alert(name + ", your test results are in.");
 
