496fa413b8ab1b871b9d2fc9e914193630cb573b
Guest User <Guest@Epicodus-10D.local>
2017-10-20 13:15:12 -0700

    debug flickering issue associated with result conclusion

--- js/scripts.js
+++ js/scripts.js
@@ -7,46 +7,48 @@ $(document).ready(function() {
     var tolerance =$("input:radio[name=tolerance]:checked").val();
     var stability =$("input:radio[name=stability]:checked").val();
 
-      if (name === "" || aesthetics === "" || syntax === "") {
-        alert("PLEASE ANSWER ALL QUESTIONS BEFORE SUBMITTING")
-        $(".results").hide();
-      }
-      if (tolerance === "high" || stability === "mid" && popularity === "high") {
-        $("#java").show();
-        $("#c-sharp").hide();
-        $("#css").hide();
-        $("#php").hide();
-        $("#ruby").hide();
-      }
-      if (tolerance === "mid" || popularity === "high" && important === "high") {
-        $("#c-sharp").show();
-        $("#java").hide();
-        $("#css").hide();
-        $("#php").hide();
-        $("#ruby").hide();
-      }
-      if (stability === "high" || popularity === "mid") {
-        $("#php").show();
-        $("#c-sharp").hide();
-        $("#java").hide();
-        $("#css").hide();
-        $("#ruby").hide();
-      }
-      if (popularity === "low" || tolerance === "low" || syntax === "yes" || stability === "low") {
-        $("#ruby").show();
-        $("#c-sharp").hide();
-        $("#java").hide();
-        $("#css").hide();
-        $("#php").hide();
-      }
-      else {
-        $("#css").show();
-        $("#c-sharp").hide();
-        $("#java").hide();
-        $("#php").hide();
-        $("#ruby").hide();
-      }
+    $(".name").text(name);
 
+    // if (name === "" || aesthetics === "" || syntax === "") {
+    //   alert("PLEASE ANSWER ALL QUESTIONS BEFORE SUBMITTING")
+    //   $(".results").hide();
+    // }
+    if (tolerance === "high" || stability === "mid" && popularity === "high") {
+      $("#java").show();
+      $("#c-sharp").hide();
+      $("#css").hide();
+      $("#php").hide();
+      $("#ruby").hide();
+    }
+    if (tolerance === "mid" || popularity === "high" && important === "high") {
+      $("#c-sharp").show();
+      $("#java").hide();
+      $("#css").hide();
+      $("#php").hide();
+      $("#ruby").hide();
+    }
+    if (stability === "high" || popularity === "mid") {
+      $("#php").show();
+      $("#c-sharp").hide();
+      $("#java").hide();
+      $("#css").hide();
+      $("#ruby").hide();
+    }
+    if (popularity === "low" || tolerance === "low" || syntax === "yes" || stability === "low") {
+      $("#ruby").show();
+      $("#c-sharp").hide();
+      $("#java").hide();
+      $("#css").hide();
+      $("#php").hide();
+    }
+    else {
+      $("#css").show();
+      $("#c-sharp").hide();
+      $("#java").hide();
+      $("#php").hide();
+      $("#ruby").hide();
+    }
+    debugger;
     event.preventDefault();
   });
 });
