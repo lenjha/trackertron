08ec31dd8ec2b157983f32358a5fd3b456c50830
Guest User <Guest@Epicodus-10D.local>
2017-10-20 15:59:33 -0700

    add fade, debug order of results

--- js/scripts.js
+++ js/scripts.js
@@ -3,54 +3,55 @@ $(document).ready(function() {
     var name = $("input#name").val();
     var aesthetics = $("select#aesthetics").val();
     var syntax = $("select#syntax").val();
+    var stability = $("select#stability").val();
     var popularity =$("input:radio[name=popularity]:checked").val();
     var tolerance =$("input:radio[name=tolerance]:checked").val();
-    var stability =$("input:radio[name=stability]:checked").val();
+    var importance =$("input:radio[name=importance]:checked").val();
 
     $(".name").text(name);
 
-    if (name === "" || aesthetics === "" || syntax === "" || stability === "") {
-      $("#c-sharp").hide();
-      $("#css").hide();
-      $("#php").hide();
-      $("#ruby").hide();
-      $("#java").hide();
-      alert("HELP ME TO HELP YOU, MEAT - ANSWER ALL THE QUESTIONS PROPERLY")
-    }
-    if (tolerance === "high" || stability === "mid" && popularity === "high" || syntax === "no" && popularity === "low") {
-      $("#java").show();
+    if (tolerance === "high" || stability === "mid" && importance === "high" || syntax === "no") {
+      $("#java").fadeIn();
       $("#c-sharp").hide();
       $("#css").hide();
       $("#php").hide();
       $("#ruby").hide();
     }
-    if (tolerance === "mid" || popularity === "high" && important === "high") {
-      $("#c-sharp").show();
+    if (tolerance === "mid" || popularity === "high" && importance === "high") {
+      $("#c-sharp").fadeIn();
       $("#java").hide();
       $("#css").hide();
       $("#php").hide();
       $("#ruby").hide();
     }
     if (stability === "high" || popularity === "mid") {
-      $("#php").show();
+      $("#php").fadeIn();
       $("#c-sharp").hide();
       $("#java").hide();
       $("#css").hide();
       $("#ruby").hide();
     }
     if (popularity === "low" || tolerance === "low" || syntax === "yes" || stability === "low") {
-      $("#ruby").show();
+      $("#ruby").fadeIn();
       $("#c-sharp").hide();
       $("#java").hide();
       $("#css").hide();
       $("#php").hide();
     }
     if (aesthetics === "yes") {
-      $("#css").show();
+      $("#css").fadeIn();
       $("#c-sharp").hide();
       $("#java").hide();
       $("#php").hide();
       $("#ruby").hide();
+    }
+    if (name === "" || aesthetics === "" || syntax === "" || popularity === "" || tolerance === "" || importance === "" || stability === "") {
+      $("#c-sharp").hide();
+      $("#css").hide();
+      $("#php").hide();
+      $("#ruby").hide();
+      $("#java").hide();
+      alert("HELP ME TO HELP YOU, MEAT - ANSWER ALL THE QUESTIONS PROPERLY")
     }
 
     event.preventDefault();
